99b21deba5497c70af95a81a8c206ce62c8b3c6f
Stephano Hong <stephano.j.hong@gmail.com>
2016-04-17 23:37:40 -0500

    Modify code to comply with Release 4

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,3 +1,5 @@
+#Methods and Pseudocodes
+
 # Making an Encrypt algorithm
 # First set index = 0
 # While the value of index is less than the length of the password
@@ -5,18 +7,17 @@
 # Add a value of one to index and loop.
 # Print out the now encrypted password
 
+
+#Shifts letters in any string to the next letter
 def encrypt(password)
     index = 0
     while index < password.length
+    #shift the letter at the position to next one, then advances the loop
     password[index] = password[index].next!
     index += 1
     end
-    puts "#{password}"
+    password = password.gsub(/ab/ , "a")
 end
-
-encrypt("abc")
-encrypt("zed") 
-
 
 # Making a Decrypt algorithm
 # Set string = "abcdefghijklmnopqrstuvwxyz"
@@ -27,16 +28,32 @@ encrypt("zed")
 # loop it so it happens with each letter as in the encrypt algorithm
 # print out the decrypted password.
 
-
+#Shifts letters in any string to one letter before
 def decrypt(password)
 	count = 0
+	#variable set to act as our key
 	string = "abcdefghijklmnopqrstuvwxyz"
 	while count < password.length
+		#set the current letter at the position to the letter before it 
+		#using the key
 		password[count] = string[string.index(password[count]) - 1]
 		count += 1
 	end
 	puts password
 end
 
+#Driver Code
+
+#Shifting letters, including edge cases
+puts encrypt("abc")
+puts encrypt("zed") 
+
+#Shifting letters back, including edge cases
 decrypt("bcd")
 decrypt("afe")
+
+#Nested Method Calling
+# the following works because the encrypt method applies its modifications to 
+# a string and saves it as a string. This string is then applied as the input
+# to the decrypt method, allowing it to carry out its function.
+decrypt(encrypt("swordfish"))
