c82d6f2ea6871761c5ce3d8d4aeef99fbe5091b8
Stephano Hong <stephano.j.hong@gmail.com>
2016-04-17 20:01:03 -0500

    Finish Release 2 and 3. Paired with Ted Smith

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,16 +1,20 @@
 puts "What is your hamster's name?"
 name = gets.chomp
 puts "What is its volume level?"
-volume = gets.chomp
+volume = gets.chomp.to_i
 puts "What is its fur color?"
-volume = gets.chomp
+color = gets.chomp
 puts "Is this hamster a good candidate for adoption?"
 adoptability = gets.chomp
 puts "Where would you estimate this hamster's age at?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 if age == ""
 	age = nil
 end
 
-puts age
+puts "Name: #{name} "
+puts "Volume level: #{volume}"
+puts "Fur color: #{color}"
+puts "Good for adoption?: #{adoptability}"
+puts "Estimated age: #{age}"
