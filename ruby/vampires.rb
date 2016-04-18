5ba79eb0fc852a347ecb359e0ebbb492b185a378
Stephano Hong <stephano.j.hong@gmail.com>
2016-04-18 08:38:02 -0500

    Finish Release 1. Add basic questions.

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old will you be at the end of the year?"
+age = gets.chomp
+puts "What year were you born?"
+year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+bread = gets.chomp
+puts "Would you like to enroll in our compnay's health insurance?"
+health = gets.chomp
+
