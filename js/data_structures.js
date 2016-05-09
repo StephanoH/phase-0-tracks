9b8bcd4a21fb4ca9801726274d97a134ca1ca879
Stephano Hong <stephano.j.hong@gmail.com>
2016-05-09 09:24:31 -0500

    Make hash that iterates over two arrays for key value pairs

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,9 @@
-var colors = [blue, green, red, yellow]
-var names = [Ed, Jon, Davos, Olly]
+var colors = ["blue", "green", "red", "yellow"];
+var names = ["Ed", "Jon", "Davos", "Olly"];
+var horseTraits = {};
+
+for (var i = 0; i < colors.length; i++) {
+	horseTraits[names[i]] = colors[i];
+}
+
+console.log(horseTraits);
