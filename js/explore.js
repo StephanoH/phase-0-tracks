4ff16e5390473ab45a5c90c38e85d49b7cdda924
Stephano Hong <stephano.j.hong@gmail.com>
2016-05-09 04:14:48 -0500

    Add explore.js and wrote program to reverse words

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// Reversing a letter in JS
+// Split word into individual letters
+// Reverse the order of the letters
+// Rejoin the split letters
+
+
+function reverse(word) {
+	return word.split('').reverse().join('');
+}
+
+var testCode = reverse("hello")
+
+for (var i = 1; i <=1; i++) {
+	console.log(testCode);
+}
