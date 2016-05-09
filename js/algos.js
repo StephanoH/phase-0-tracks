d8616a1d595056ae39d1b11df44c468274c376f1
Stephano Hong <stephano.j.hong@gmail.com>
2016-05-09 11:33:19 -0500

    add matching_info, which searches for matching key value pairs in a given set of objects (release 2)

--- js/algos.js
+++ js/algos.js
@@ -19,4 +19,41 @@ function LongestPhrase(array) {
 	console.log(this.array);
 }
 
+// Function to search for matching key-value pairs
+// 1. setup a loop to iterate through each elements of the first hash
+// 2. for each iteration, look for a match in the second hash
+// 3. if match found, return true (maybe set true to a new variable) and stop
+// 4. if none found, return false and continue onto next element
+
+function Matching_info(first_data, second_data) {
+	this.first_data = first_data;
+	this.second_data = second_data;
+	key_match = false;
+	value_match = false;
+
+	for (var i = 0; i < Object.keys(first_data).length; i++) {
+		if (Object.keys(first_data)[i] in second_data) {
+			key_match = true;
+			if (this.first_data[Object.keys(first_data)[0]] == this.second_data[Object.keys(first_data)[0]]) {
+				value_match = true;
+			} else {
+				value_match = false;
+			}
+		} else {
+			key_match = false;
+		}
+
+	}
+
+	if (key_match && value_match) {
+		console.log("true");
+	} else {
+		console.log("false");
+	}
+}
+
+
+// Driver Code
 var test = new LongestPhrase(["a", "ab", "abc", "abcd"])
+var test_match = new Matching_info({name: "Steven", age: 54}, {name: "Steven", age: 24})
+var test_match_false = new Matching_info({name: "Andrew", age: 13}, {name: "Amanda", age: 1423})
